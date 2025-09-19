498e0119a6150abaafcfbd84f6e47fbe33fb3a61
temotec <baldomeroaguilarramos@gmail.com>
2025-09-18 23:44:41 -0600

    first commit

---
+++ ruta_javascript_platzi/fundamentos_de_javascript/clase-variables.js
@@ -0,0 +1,12 @@
+let cajaDeAndy = "wooody";
+console.log(cajaDeAndy);
+
+//  Lo No permitido
+let c = "woody"
+let cda = "buzz"
+let pcAndy = "jessie"
+
+// Lo permitido
+let primerTrasteodeAndy = "woody"
+let urlDelUsuario = "https://www.google.com"
+let idDelUsuario = "12345"
