dd4d7ff485b9e3fc311508bcaa986e49c59131ea
temotec <baldomeroaguilarramos@gmail.com>
2025-09-29 11:51:46 -0600

    feat: added my course files about javascript -baldo

---
+++ ruta_javascript_platzi/fundamentos_de_javascript/Primitivos_y_complejos/tipos_de_datos.js
@@ -0,0 +1,38 @@
+//PRIMITIVOS
+
+//STRING 
+let nombre = "Temo"
+
+//NUMBER
+let edad = 30
+
+//BOOLEAN
+let esMayorDeEdad = true
+
+//NULL
+let noHatValor = null
+
+//UNDEFINED
+let noDefinido = undefined
+
+//SYMBOL
+let simbolo = Symbol("mi simbolo")
+
+//BIGINT
+let numeroGrande = 9007199254740991n
+
+//COMPLEJOS
+
+//OBJECT
+let persona = {
+    nombre: "Temo",
+    edad: 30
+}
+
+//ARRAY
+let frutas = ["Manzana", "Pera", "Uva"]
+
+//FUNCTION
+function saludar() {
+    console.log("Hola")
+}
---
+++ ruta_javascript_platzi/fundamentos_de_javascript/strings.js
@@ -0,0 +1,12 @@
+// Manipulacion de strings en javascript: escritura, concentacion y substrings
+
+// Tipo de dato: String
+let string1 = 'Hoal Mundo'
+let string2 = "JavaScript es genial"
+let string3 = `${string1} feliz :)`
+let string4 = string1 + ' ' + string2
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
