5deb27ac0e754e8d880b2f04a69ae9bed3ab3ab2
temotec <baldomeroaguilarramos@gmail.com>
2026-03-31 20:11:11 -0600

    feat: i modified file clase de tipos -baldo

--- ruta_javascript_platzi/fundamentos_de_javascript/clase-conversion-de-tipos.js
+++ ruta_javascript_platzi/fundamentos_de_javascript/clase-conversion-de-tipos.js
@@ -5,3 +5,12 @@ const integer = parseInt(string); // Convierte a entero
 console.log(integer);
 console.log(typeof integer);
 
+const stringDecimal = '3.14';
+const float = parseFloat(stringDecimal); // Convierte a flotante
+console.log(float);
+console.log(typeof float);
+
+const binary = '1010';
+const decimal = parseInt(binary, 2); // Convierte a decimal desde binario
+console.log(decimal);
+console.log(typeof decimal);
